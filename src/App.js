206078418cde7a49b7b9c84f415d050412b799b3
will staunton <will.staunton@reedonline.co.uk>
2020-10-02 07:55:46 +0100

    setting up template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  console.log(this);
+  return (
+    <div className="App">
+      <h1>Template</h1>
+    </div>
+  );
+}
+
+export default App;
